74bf4de7d415cc20b61172aa3b16b5b7bb9dab5b
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2025-01-01 17:29:48 +1100

    update fetch url

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -84,7 +84,7 @@ const Dashboard = () => {
         setloading(true)
         seterror("")
         try {
-            const response = await fetch('http://127.0.0.1:8000/summarize', {
+            const response = await fetch('http://13.211.6.27:8000/summarize', {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
