23ff361922b6331dedca69d4e36f9c677590d6f5
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2025-01-01 18:15:26 +1100

    chnge

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -84,7 +84,7 @@ const Dashboard = () => {
         setloading(true)
         seterror("")
         try {
-            const response = await fetch('http://127.0.0.1:8000/summarize', {
+            const response = await fetch('http://3.25.84.115:8000/summarize', {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
